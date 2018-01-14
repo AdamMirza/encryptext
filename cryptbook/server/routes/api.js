d480df1c4cce55c32f1c37f9fdde4ec465c86940
Ebou Jobe <ebou.jobe@mail.mcgill.ca>
2018-01-14 04:37:13 -0500

    implemented the res api for for get username

--- cryptbook/server/routes/api.js
+++ cryptbook/server/routes/api.js
@@ -1,12 +1,11 @@
-
 const express = require("express");
 var MongoClient = require("mongodb").MongoClient;
 var ObjectId = require("mongodb").ObjectID;
 var dbName = "EncrytText";
 var dbpassword = "ouwnhEPf2Vf2VfMgfQG";
 const router = express.Router();
-const keyGen = require('../../modules/keyGen')
-var bodyParser = require('body-parser')
+const keyGen = require("../../modules/keyGen");
+var bodyParser = require("body-parser");
 
 var url =
   "mongodb://get-secured:ouwnhEPf2VfMgfQG@cluster0-shard-00-00-bwprg.mongodb.net:27017,cluster0-shard-00-01-bwprg.mongodb.net:27017,cluster0-shard-00-02-bwprg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
@@ -17,32 +16,68 @@ router.get("/api", function(req, res) {
       res.send("Error connecting " + err);
       return;
     }
+    var db = db.db("EncrytText");
+    var myobj = {
+      Name: "Company Inc",
+      email: "Highway 37",
+      public_key: "ebgwxduy",
+      password: "lol",
+      private_key: "xeg927d297g",
+      contacts: { eb: "echn", key: "exhb " }
+    };
+    db.collection("Users").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      return;
+    });
+
     res.send("connected successfully to database");
   });
 });
 
 // Connect to the db
 
-router.get('/test',async function(req, res) {
+router.get("/test", async function(req, res) {
   let key = await keyGen.newKeyPair();
   privkey = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
-  pubkey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+  pubkey = key.publicKeyArmored; // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
   let data = {
-    'public_key': pubkey,
-    'private_key':privkey
-  }
-  res.send(data)
-})
+    public_key: pubkey,
+    private_key: privkey
+  };
+  res.send(data);
+});
 
+router.get("/api/Users/:username", function(req, res) {
+  let data = {
+    _id: "",
+    username: req.params.username,
+    public_key: "",
+    contacts: {}
+  };
 
-router.get('/api/user/:username', function(req, res){
-    let data = {
-      '_id':12345,
-      'username':req.params.username,
-      'public_key':'-----BEGIN PGP PUBLIC KEY BLOCK ... '
+  MongoClient.connect(url, (err, db) => {
+    if (err) {
+      res.send("Error connecting " + err);
+      return;
     }
-    res.json(data);
-})
-
+    var db = db.db("EncrytText");
+    var query = { username: req.params.username };
+    db
+      .collection("Users")
+      .find(query)
+      .toArray(function(err, result) {
+        if (err) {
+          throw err;
+        }
+        data["_id"] = result._id;
+        data["public_key"] = result.public_key;
+        data["contacts"] = result.contacts;
+      });
+    //res.send("connected successfully to database");
+  });
+  res.json(data);
+  //res.send(data);
+});
 
 module.exports = router;
