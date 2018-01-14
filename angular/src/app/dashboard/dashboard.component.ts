a13834c4d592bd5141605cda775381e4aaadf6cd
AdamCollins <adamcollinssecure@gmail.com>
2018-01-14 04:08:06 -0800

    added session

--- angular/src/app/dashboard/dashboard.component.ts
+++ angular/src/app/dashboard/dashboard.component.ts
@@ -12,4 +12,30 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
   }
 
+
+}
+
+function getSession() {
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  let cookie = getCookie('session')
+  let session = JSON.parse(cookie)
+  console.log(session)
+  if(session)
+    return session
+  else
+    return null;
 }
