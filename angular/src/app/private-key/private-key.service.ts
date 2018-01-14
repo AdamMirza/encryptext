49deb34a81dfcbde20a6faa2c586e9d1d8dfef9c
Adam Mirza <adammirza12@hotmail.com>
2018-01-14 02:21:25 -0800

    Added service for private key

--- angular/src/app/private-key/private-key.service.ts
+++ angular/src/app/private-key/private-key.service.ts
@@ -2,7 +2,22 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class PrivateKeyService {
+  visible: boolean;
+  key: string;
 
-  constructor() { }
+  constructor(private key: string) { this.visible = false; }
+
+  show() {
+    this.visible = true;
+  }
+
+  hide() {
+    this.visible = false;
+  }
+
+  toggle() {
+    this.visible = !this.visible;
+  }
+
 
 }
