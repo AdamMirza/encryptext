e26a42a1242e4073a424712e3436413b77cbdea6
Adam Mirza <adammirza12@hotmail.com>
2018-01-13 21:11:45 -0800

    Created necessary initial core components for app (private key, header, dashboard, footer, and login form).

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PrivateKeyComponent } from './private-key/private-key.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrivateKeyComponent,
+    DashboardComponent,
+    FooterComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
---
+++ angular/src/app/private-key/private-key.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-key',
+  templateUrl: './private-key.component.html',
+  styleUrls: ['./private-key.component.css']
+})
+export class PrivateKeyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
