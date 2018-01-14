52c703a57fc37a168ba7c5da5e5da7ecd6286d1f
Adam Mirza <adammirza12@hotmail.com>
2018-01-14 07:46:58 -0800

    Included font awesome for icons

--- angular/src/app/contact-table/contact-table.component.ts
+++ angular/src/app/contact-table/contact-table.component.ts
@@ -27,6 +27,38 @@ export class ContactTableComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let user = getSession();
+
+    if (user == null) {
+      throw Exception;
+    }
+
+    console.log(user[0].username);
+  }
+
+  function getSession() {
+    function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+    let cookie = getCookie('session');
+    let session = JSON.parse(cookie);
+    console.log(session);
+    if(session)
+      return session;
+    else
+      return null;
   }
 
 }
