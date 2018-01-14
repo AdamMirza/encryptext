1f5c65e7046b5ae3282d85e628ea388b3bbeedf2
Adam Mirza <adammirza12@hotmail.com>
2018-01-13 17:34:44 -0800

    Added capability for login (hardcoding admin mode). Not fully functional yet, need API for mongo login.

--- cryptbook/src/app/app.module.ts
+++ cryptbook/src/app/app.module.ts
@@ -3,13 +3,35 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: LoginFormComponent
+  }
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    FooterComponent,
+    DashboardComponent,
+    AdminDashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -7,6 +7,7 @@ export class UserDataService {
   constructor() { }
 
   addContact(user: User) {
+    
   }
 
 }
