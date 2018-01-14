4ebe3028c4374df833e5ed9e04c913ca7d0c791e
Ebou Jobe <ebou.jobe@mail.mcgill.ca>
2018-01-13 21:57:41 -0500

    Successfully connected to mongoDb

--- cryptbook/server/routes/api.js
+++ cryptbook/server/routes/api.js
@@ -1,9 +1,23 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+var MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectID;
+var dbName = "EncrytText";
+var dbpassword = "ouwnhEPf2Vf2VfMgfQG";
+const router = express.Router();
 
-router.get('/api', function(req, res) {
-  res.send('api page')
-})
+var url =
+  "mongodb://get-secured:ouwnhEPf2VfMgfQG@cluster0-shard-00-00-bwprg.mongodb.net:27017,cluster0-shard-00-01-bwprg.mongodb.net:27017,cluster0-shard-00-02-bwprg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+//mongoose.connect('mongodb://username:password@host:port/database')
+router.get("/api", function(req, res) {
+  MongoClient.connect(url, (err, db) => {
+    if (err) {
+      res.send("Error connecting " + err);
+      return;
+    }
+    res.send("connected successfully to database");
+  });
+});
 
+// Connect to the db
 
-module.exports = router
+module.exports = router;
